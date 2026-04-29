6fab3e80cc2ef55274a81621a438b83f60184536
letangileri <let.angileri@gmail.com>
2026-04-29 18:37:40 +0200

    Implement autocomplete feature with product suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  console.log(suggestions);
+
+  useEffect(() => {
+    if (query.trim() === "") {
+      setSuggestions([]);
+      return;
+    }
+    fetch(`http://localhost:3333/products?search=${query}`)
+      .then((res) => res.json())
+      .then((data) => setSuggestions(data));
+  }, [query]);
+
   return (
     <div>
       <h1>Autocomplete</h1>
       <input
         type="text"
         placeholder="cerca i prodotti"
-        value={(e) => e.target.value}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
+      {suggestions.length > 0 && (
+        <div className="dropdown">
+          {suggestions.map((product) => (
+            <p key={product.id}>{product.name}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
